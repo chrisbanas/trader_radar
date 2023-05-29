148344a1a00d7727aee4f3b4fab2ed11df26d4cf
chrisbanas <bananas595@gmail.com>
2023-05-29 11:56:53 -0700

    updated clock function to handle holidays for the next 20 yrs

--- src/scripts/time.js
+++ src/scripts/time.js
@@ -1,4 +1,31 @@
 export default function updateTime() {
+
+    // A list of all stock market holidays in the format 'Month Date for next 20 yrs'
+    let marketHolidays = {
+        '2023': ['Jan 2', 'Jan 16', 'Feb 20', 'May 29', 'Jul 4', 'Sep 4', 'Nov 23', 'Dec 25'],
+        '2024': ['Jan 1', 'Jan 15', 'Feb 19', 'May 27', 'Jul 4', 'Sep 2', 'Nov 28', 'Dec 25'],
+        '2025': ['Jan 1', 'Jan 20', 'Feb 17', 'May 26', 'Jul 4', 'Sep 1', 'Nov 27', 'Dec 25'],
+        '2026': ['Jan 1', 'Jan 19', 'Feb 16', 'May 25', 'Jul 3', 'Sep 7', 'Nov 26', 'Dec 25'],
+        '2027': ['Jan 1', 'Jan 18', 'Feb 15', 'May 31', 'Jul 5', 'Sep 6', 'Nov 25', 'Dec 24'],
+        '2028': ['Jan 2', 'Jan 16', 'Feb 20', 'May 29', 'Jul 4', 'Sep 4', 'Nov 23', 'Dec 25'],
+        '2029': ['Jan 1', 'Jan 15', 'Feb 19', 'May 28', 'Jul 4', 'Sep 3', 'Nov 22', 'Dec 25'],
+        '2030': ['Jan 1', 'Jan 21', 'Feb 18', 'May 27', 'Jul 4', 'Sep 2', 'Nov 28', 'Dec 25'],
+        '2031': ['Jan 1', 'Jan 20', 'Feb 17', 'May 26', 'Jul 4', 'Sep 1', 'Nov 27', 'Dec 25'],
+        '2032': ['Jan 1', 'Jan 19', 'Feb 16', 'May 31', 'Jul 5', 'Sep 6', 'Nov 25', 'Dec 24'],
+        '2033': ['Jan 1', 'Jan 17', 'Feb 21', 'May 30', 'Jul 4', 'Sep 5', 'Nov 24', 'Dec 26'],
+        '2034': ['Jan 2', 'Jan 16', 'Feb 20', 'May 29', 'Jul 4', 'Sep 4', 'Nov 23', 'Dec 25'],
+        '2035': ['Jan 1', 'Jan 15', 'Feb 19', 'May 28', 'Jul 4', 'Sep 3', 'Nov 22', 'Dec 25'],
+        '2036': ['Jan 1', 'Jan 21', 'Feb 18', 'May 26', 'Jul 4', 'Sep 1', 'Nov 27', 'Dec 26'],
+        '2037': ['Jan 1', 'Jan 19', 'Feb 16', 'May 25', 'Jul 3', 'Sep 7', 'Nov 26', 'Dec 25'],
+        '2038': ['Jan 1', 'Jan 18', 'Feb 15', 'May 31', 'Jul 5', 'Sep 6', 'Nov 25', 'Dec 24'],
+        '2039': ['Jan 2', 'Jan 16', 'Feb 20', 'May 29', 'Jul 4', 'Sep 4', 'Nov 23', 'Dec 25'],
+        '2040': ['Jan 1', 'Jan 15', 'Feb 19', 'May 27', 'Jul 4', 'Sep 2', 'Nov 28', 'Dec 25'],
+        '2041': ['Jan 1', 'Jan 20', 'Feb 17', 'May 26', 'Jul 4', 'Sep 1', 'Nov 27', 'Dec 25'],
+        '2042': ['Jan 1', 'Jan 19', 'Feb 16', 'May 31', 'Jul 5', 'Sep 6', 'Nov 25', 'Dec 24'],
+        '2043': ['Jan 1', 'Jan 18', 'Feb 15', 'May 30', 'Jul 4', 'Sep 5', 'Nov 24', 'Dec 26']
+    };
+
+    // clock options
     let options = {
         weekday: 'short',
         month: 'short',
@@ -10,9 +37,15 @@ export default function updateTime() {
         timeZone: 'America/New_York',
     };
 
+    // create a new date string
     let dt = new Date();
     let dateTimeString = dt.toLocaleString('en-US', options);
     dateTimeString = dateTimeString.replace(/([A-Z]+) Standard Time$/, '$1');
+
+    // Check if current day is a market holiday
+    let dtString = dt.toLocaleString('en-US', { month: 'short', day: 'numeric' });
+    let year = dt.getFullYear().toString(); // get current year as string
+    let isMarketHoliday = marketHolidays[year].includes(dtString);
 
     // Calculate time left until the market opens or closes
     let openTime = new Date(Date.UTC(dt.getFullYear(), dt.getMonth(), dt.getDate(), 13, 30, 0)); // Market opens at 9:30 AM EST (which is 1:30 PM UTC)
@@ -20,7 +53,11 @@ export default function updateTime() {
     let timeDiff = openTime - dt;
     let timeLeft = '';
 
-    if (timeDiff > 0) { // Market is not open yet
+    // sets the open or close message
+    if (isMarketHoliday) {
+        timeLeft = 'Market is closed';
+    }
+    else if (timeDiff > 0) { // Market is not open yet
         timeLeft = Math.floor(timeDiff / 1000 / 60 / 60) + ' hr ' + Math.floor((timeDiff / 1000 / 60) % 60) + ' min until open';
     } else if (dt < closeTime) { // Market is open
         timeDiff = closeTime - dt;
@@ -29,10 +66,12 @@ export default function updateTime() {
         timeLeft = 'Market is closed';
     }
 
-    let nyTime = new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
+
+    // sets the color to green on open and red on closed
+    let nyTime = new Date().toLocaleString("en-US", { timeZone: "America/New_York" });
     let nyHour = new Date(nyTime).getHours();
 
-    if (nyHour >= 9 && nyHour < 16) {
+    if (!isMarketHoliday && nyHour >= 9 && nyHour < 16) {
         // Market is open - set text color to green
         dateTimeString += ' EST  |  ' + timeLeft;
         document.getElementById("datetime").style.color = "green";
